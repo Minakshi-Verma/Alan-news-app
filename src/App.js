737d8beb7bd20e799b58c0632b45192cb9d9bcd8
Minakshi Verma <mvedwan@gmail.com>
2020-10-02 16:12:25 -0400

    Alan software is intergrated with react app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
+//alan API key generated after integrating with alan ai software(alan studio)
+const alanKey = '871bb42135775d06b86906a4b85681df2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+
+    //initialize the alanbtn
+    useEffect(()=>{
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command})=> {
+              if(command ==='testCommand'){
+                  alert('This code was executed')
+              }   
+            }
+        })
+    }, [])
+
      return(
          <div>  
              <h1>Alan AI News Application</h1>
