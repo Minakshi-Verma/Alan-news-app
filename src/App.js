cb5cd60ab6e2c28a047dbd57cbd704e9ca0bdba5
Minakshi Verma <mvedwan@gmail.com>
2020-10-03 16:18:56 -0400

    News is displaying on cards using voice command.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 
+import NewsCards from './components/NewsCards/NewsCards';
+
+
 //alan API key generated after integrating with alan ai software(alan studio)
 const alanKey = '871bb42135775d06b86906a4b85681df2e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
@@ -12,7 +15,7 @@ const App = () => {
             key: alanKey,
             onCommand: ({command, articles})=> {
               if(command ==='newHeadlines'){
-                  setNewsArticles(articles)                  
+                  setNewsArticles(newsArticles)                  
               }   
             }
         })
@@ -21,6 +24,7 @@ const App = () => {
      return(
          <div>  
              <h1>Alan AI News Application</h1>
+             <NewsCards articles = {articles}/>
          </div>
      )
 }
