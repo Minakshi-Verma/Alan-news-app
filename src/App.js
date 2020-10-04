5643d099f894c6431714fdccb00906bd4106ed05
Minakshi Verma <mvedwan@gmail.com>
2020-10-04 19:22:59 -0400

    Added go back  and opening link functionality!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
 import useStyle from './styles'
+
 
 
 //alan API key generated after integrating with alan ai software(alan studio)
@@ -14,19 +16,38 @@ const App = () => {
     const [newsArticles, setNewsArticles] = useState([])
     //We are starting from index 0 since this would be the index he would be currently reading
     const [activeArticle, setActiveArticle] = useState(0)
+    
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand: ({command, articles})=> {
-              if(command ==='newHeadlines'){
-                  setNewsArticles(articles)                  
-              }  
-              else if(command==='highlight') {
-                  //We will change the state based on previous state
-                  //we set it by calling a callback that takes prevstate and gives the new one
-                  //React recommends, if you are changing the state based on prev state, you call it as a  callback fx
-                setActiveArticle((prevActiveArticle)=>prevActiveArticle + 1)
-              }
+            onCommand: ({command, articles, number})=> {
+                if(command ==='newHeadlines'){
+                    setNewsArticles(articles);
+
+                    //reset the activeArticle eachtime
+                    //   setActiveArticle(-1);              
+                }  
+                else if(command==='highlight') {
+                    //We will change the state based on previous state
+                    //we set it by calling a callback that takes prevstate and gives the new one
+                    //React recommends, if you are changing the state based on prev state, you call it as a  callback fx
+                    setActiveArticle((prevActiveArticle)=>prevActiveArticle + 1)
+                }
+                else if({command:'open'}){
+                    //fuzzy: true, will help to parse the word closest to that number
+                    //for ===> four ==> 4
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy:true}): number
+                    const article = articles[parsedNumber -1];
+
+                    //we can only play 20 articles, make a check if we exceed the limit
+                    if (parsedNumber>20){
+                        alanBtn().playText('Please try that again')
+                    }else if(article){
+                        //open a new website based on link
+                        window.open(articles[parsedNumber].url,'_blank') 
+                        alanBtn().playText('Opening the article')
+                    }                   
+                }
             }
         })
     }, [])
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { createRef, useState, useEffect } from 'react';
 import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
 import useStyles from './styles.js';
+import classNames from 'classnames'
 
 const NewsCard = ({article:{description, publishedAt, source, title, url, urlToImage }, i, activeArticle}) =>{
     const classes = useStyles()
+    //we need to set the ref to all our active cards to make the scrol work
+    const [elRefs, setElRefs] = useState([])
+    const scrollToRef = (ref)=> window.scroll(0, ref.current.offsetTop - 50); //x axis=0
+
+
+    //We need to have refernce for all the cards, if we donot have it, we need to create one
+    //We are going to do this only once when our component first mount
+    useEffect(()=>{
+        setElRefs((refs)=>Array(20).fill().map((_,j)=>refs[j] || createRef()));
+    },[])
+
+    //Now we have all the refs and we can scroll through them everytime
+    //this useEffect will run everytime it reads an article and will run if there is any update in i, activearticle or elRefs
+    useEffect(()=>{
+        if(i===activeArticle && elRefs[activeArticle]){
+            scrollToRef(elRefs[activeArticle])
+        }
+    }, [i, activeArticle, elRefs])
 
     return (
-        <Card className={classes.card}>
+        <Card ref = {elRefs[i]} className={classNames(classes.card, activeArticle ===i+1 ? classes.activeCard:null)}>
             <CardActionArea href={url} target="_blank">  
                 <CardMedia className={classes.media} image={urlToImage || 'https://www.indiafellow.org/blog/wp-content/uploads/2020/05/Marketplace-Lending-News.jpg'} />
                 <div className={classes.details}>
@@ -20,7 +39,7 @@ const NewsCard = ({article:{description, publishedAt, source, title, url, urlToI
             </CardActionArea>
             <CardActions className={classes.cardActions}>
                 <Button size='small' color='primary'>Learn More</Button>                
-                <Typography variant={"h5"} color={"textSecondary"} >Go to next { i + 1 }</Typography>
+                <Typography variant={"body2"} color={"textSecondary"} > Article: { i + 1 }</Typography>
             </CardActions>
         </Card>
     )
